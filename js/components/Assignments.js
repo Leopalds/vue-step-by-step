f946a79faf1f4c7cd96d3f775dbd7ddb8f3515ef
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-05-25 15:48:50 -0300

    ep 8- Handle a Form Submission

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -8,6 +8,13 @@ export default {
     <section class="space-y-6">
         <assignmentList :assignments="filters.inProgress" title="In Progress" ></assignmentList>
         <assignmentList :assignments="filters.completed" title="Completed"></assignmentList>
+
+        <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black">
+                <input v-model="newAssignment" type="text" placeholder="New Assignment..." class="p-2" />
+                <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+        </form>
     </section>
     `,
     data(){
@@ -16,7 +23,8 @@ export default {
                 {name: 'Finish Project', complete: false, id: 1},
                 {name: 'Read Chapter 4', complete: false, id: 2},
                 {name: 'Turn in Homerwork', complete: false, id: 3},
-            ]
+            ],
+            newAssignment: '',
         }
     },
     computed: {
@@ -25,6 +33,17 @@ export default {
                 inProgress: this.assignments.filter(assignment => ! assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete),
             }
+        }
+    },
+    methods: {
+        add(){
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1
+            });
+
+            this.newAssignment = '';
         }
     }
 }
